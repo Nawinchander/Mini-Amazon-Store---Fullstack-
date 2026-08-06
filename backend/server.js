a0a210afdb1bd93bc297a012ab885416bfe8e8b1
Nawin chander <nawinnapzy289@gmail.com>
2026-08-06 09:49:20 +0530

    ad

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,10 @@ const connectSQL = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+connectMongo();
+connectSQL();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
