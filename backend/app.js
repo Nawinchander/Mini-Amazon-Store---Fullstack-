4baf46fd897eb539198e593af285e4d73695f07a
Nawin chander <nawinnapzy289@gmail.com>
2026-08-06 09:49:50 +0530

    add

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const productRoutes = require("./routes/product.routes");
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Mini Amazon API Running"
+    });
+});
+
+
