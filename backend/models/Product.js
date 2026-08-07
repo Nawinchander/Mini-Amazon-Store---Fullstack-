4f844d9d13d8a798eb247729e8ea5d176a8e9ff9
Nawin chander <nawinnapzy289@gmail.com>
2026-08-07 14:29:30 +0530

    add

---
+++ backend/models/Product.js
@@ -0,0 +1,22 @@
+/// create product collection
+
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+
+    name: String,
+
+    price: Number,
+
+    category: String,
+
+    image: String
+
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
+
+
+
+
+
