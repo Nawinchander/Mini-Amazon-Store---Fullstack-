bcbfafff4c69e4653960a8df5ea7b834b849a59b
Nawin chander <nawinnapzy289@gmail.com>
2026-08-07 14:30:39 +0530

    add

--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -3,6 +3,19 @@
 
 ///// Business Logic
 
+const Product = require("../models/Product");
+
+const getProducts = async () => {
+    return await Product.find();
+};
+
+const addProduct = async (data) => {
+    return await Product.create(data);
+};
+
+module.exports = { getProducts, addProduct };
+
+
 
 
 
