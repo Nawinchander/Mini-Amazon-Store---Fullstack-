e4bf63cfe29809981bc4db6834a0daa4358ebbf5
Nawin chander <nawinnapzy289@gmail.com>
2026-08-07 14:28:24 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,27 @@
 /// mysql 
 
 
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+const connectSQL = () => {
+    db.connect((err) => {
+        if (err) {
+            console.log("MySQL Connection Failed");
+        } else {
+            console.log("MySQL Connected");
+        }
+    });
+};
+
+module.exports = connectSQL;
+
+
 
 
