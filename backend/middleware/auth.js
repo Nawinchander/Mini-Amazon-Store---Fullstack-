85ac09e091fa8c347a7cd4538b71401c03623cfe
Nawin chander <nawinnapzy289@gmail.com>
2026-08-08 19:09:32 +0530

    ad

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,42 @@
 //// Verify JWT Token.
 
 
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+
+    const token = req.headers.authorization;
+
+    if (!token) {
+
+        return res.status(401).json({
+
+            message: "No Token"
+
+        });
+
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err) => {
+
+        if (err) {
+
+            return res.status(403).json({
+
+                message: "Invalid Token"
+
+            });
+
+        }
+
+        next();
+
+    });
+
+};
+
+module.exports = auth;
+
 
 
 
