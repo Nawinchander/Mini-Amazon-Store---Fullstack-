f826d4b799ed80e3812e5934c7d61bb31f10b0d7
Nawin chander <nawinnapzy289@gmail.com>
2026-08-08 19:07:35 +0530

    add

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,6 +3,24 @@
 ////  Simple Login API.
 
 
+const jwt = require("jsonwebtoken");
+
+const login = (req, res) => {
+
+    const token = jwt.sign({ user: "admin" }, process.env.JWT_SECRET );
+
+    res.json({
+
+        message: "Login Successful",
+
+        token
+
+    });
+
+};
+
+module.exports = { login};
+
 
 
 
