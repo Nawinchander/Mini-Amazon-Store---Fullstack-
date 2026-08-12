4f60662005a119847f15ba8da7fced823c3bf24c
Nawin chander <nawinnapzy289@gmail.com>
2026-08-12 13:31:27 +0530

    ad

---
+++ frontend/frontend-vue/src/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <input
+        type="text"
+        placeholder="Search products..."
+        :value="search"
+        @input="updateSearch"
+    />
+
+</template>
+
+<script setup>
+
+defineProps({
+    search: String
+});
+
+const emit = defineEmits([
+    "update:search"
+]);
+
+const updateSearch = (event) => {
+
+    emit(
+        "update:search",
+        event.target.value
+    );
+
+};
+
+</script>
+
+
