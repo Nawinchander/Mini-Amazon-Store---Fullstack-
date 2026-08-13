674f0313496b8a749b15bcc8830274738d86ca53
Nawin chander <nawinnapzy289@gmail.com>
2026-08-13 11:15:58 +0530

    ad

---
+++ frontend/frontend-vue/src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <div class="login">
+
+        <h1>Mini Amazon</h1>
+
+        <h2>Login</h2>
+
+        <button
+            @click="login"
+            :disabled="loading"
+        >
+
+            {{ loading ? "Logging in..." : "Login" }}
+
+        </button>
+
+    </div>
+
+</template>
+
+<script setup>
+
+import { ref } from "vue";
+
+import { loginUser } from "../services/api";
+
+const emit = defineEmits([
+    "login"
+]);
+
+const loading = ref(false);
+
+const login = async () => {
+
+    try {
+
+        loading.value = true;
+
+        const response = await loginUser();
+
+        localStorage.setItem(
+            "token",
+            response.data.token
+        );
+
+        emit("login");
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        loading.value = false;
+
+    }
+
+};
+
+</script>
+
+
