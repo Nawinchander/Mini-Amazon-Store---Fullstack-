5b25948ad62b0785d0092dab4a2f5c4867509cd3
Nawin chander <nawinnapzy289@gmail.com>
2026-08-14 07:34:23 +0530

    ad

--- frontend/frontend-vue/src/App.vue
+++ frontend/frontend-vue/src/App.vue
@@ -1,7 +1,39 @@
+<template>
+
+    <Login
+        v-if="!loggedIn"
+        @login="loggedIn = true"
+    />
+
+    <Dashboard
+        v-else
+    />
+
+</template>
+
+
 <script setup>
+
+import { ref } from "vue";
+
+import Login
+    from "./views/Login.vue";
+
+import Dashboard
+    from "./views/Dashboard.vue";
+
+
+const loggedIn = ref(
+    !!localStorage.getItem("token")
+);
+
+</script>
+
+
+<!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld />
-</template>
+</template> -->
