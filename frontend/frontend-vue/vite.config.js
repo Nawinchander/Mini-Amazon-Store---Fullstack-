b823b6cb9e1f967d099ee1407d1d3de42047f50a
Nawin chander <nawinnapzy289@gmail.com>
2026-08-17 17:39:25 +0530

    ad

--- frontend/frontend-vue/vite.config.js
+++ frontend/frontend-vue/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [react()],
+
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./tests/setup.js"
+    }
+});
+
+
