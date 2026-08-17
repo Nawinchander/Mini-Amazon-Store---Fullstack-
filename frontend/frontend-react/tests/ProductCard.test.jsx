0218f730b42b24f56d72f511bd3365f70aec2322
Nawin chander <nawinnapzy289@gmail.com>
2026-08-17 17:42:39 +0530

    ad

---
+++ frontend/frontend-react/tests/ProductCard.test.jsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+
+import ProductCard from "../src/components/ProductCard";
+
+describe("ProductCard", () => {
+
+    test("displays product information", () => {
+
+        const product = {
+            name: "iPhone 16",
+            price: 89999,
+            category: "Mobile",
+            image: "iphone.jpg"
+        };
+
+        render(
+            <ProductCard product={product} />
+        );
+
+        expect(
+            screen.getByText("iPhone 16")
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText("Mobile")
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText("₹89999")
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByRole("button", {
+                name: "Add to Cart"
+            })
+        ).toBeInTheDocument();
+
+    });
+
+});
+
+
+
+
+
+
