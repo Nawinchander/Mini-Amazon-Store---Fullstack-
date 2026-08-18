220fcab2474bfaf5726b5eae61875ef625c48d4d
Nawin chander <nawinnapzy289@gmail.com>
2026-08-18 15:09:49 +0530

    ad

--- frontend/frontend-react/tests/SearchBar.test.jsx
+++ frontend/frontend-react/tests/SearchBar.test.jsx
@@ -1,10 +1,39 @@
-
-import { render, screen, fireEvent} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import SearchBar from "../src/components/SearchBar";
 
+describe("SearchBar", () => {
 
+    test("allows user to enter search text", () => {
 
+        let search = "";
+
+        const setSearch = (value) => {
+            search = value;
+        };
+
+        render(
+            <SearchBar
+                search={search}
+                setSearch={setSearch}
+            />
+        );
+
+        const input = screen.getByPlaceholderText(
+            "Search products..."
+        );
+
+        fireEvent.change(input, {
+            target: {
+                value: "iphone"
+            }
+        });
+
+        expect(search).toBe("iphone");
+
+    });
+
+});
 
 
 
