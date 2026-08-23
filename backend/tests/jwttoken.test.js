5c56f2e62b3b97296b15d877609b1ebc3f6ac796
Nawin chander <nawinnapzy289@gmail.com>
2026-08-23 17:01:16 +0530

    ad

---
+++ backend/tests/jwttoken.test.js
@@ -0,0 +1,15 @@
+// .set(
+//     "Authorization",
+//     process.env.TEST_TOKEN
+// )
+
+
+// requires a valid test token.
+
+// For a simple project, I recommend making your authentication middleware accept a dedicated test token only when NODE_ENV === "test".//
+
+
+
+
+
+
